b0644c8a5c9955e195f80f6f33787361d12b5324
Melyssa Santos <salvadormelyssa2@gmail.com>
2025-09-04 11:46:40 -0300

    Modificando o método para receber o array

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 let button = document.getElementById('btnUsers');
+let div = document.getElementById('out');
 
 button.addEventListener('click', function () {
     // ASSINCRONA
-    fetch('https://jsonplaceholder.typicode.com/posts/1')
+    fetch('https://jsonplaceholder.typicode.com/posts')
 
     .then(function (response) {
         return response.json();
     })
 
-    .then(function (json) {
-        console.log(json);
+    .then(function (data) {
+        div.textContent = `Title: ${data[0].title}, Body: ${data[0].body}`
+        let lista0rdenada = "<ol>";
+        for (usuario in data){
+            
+        }
     })
-    // console.log("Clicou!");
 });
 
 // // SINCRONA
