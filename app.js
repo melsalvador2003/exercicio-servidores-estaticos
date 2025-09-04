08f470e3f13595125f5aa5ef4f0350de4a573026
Melyssa Santos <salvadormelyssa2@gmail.com>
2025-09-04 11:16:47 -0300

    Adicionando as funções

--- app.js
+++ app.js
@@ -3,8 +3,14 @@ let button = document.getElementById('btnUsers');
 button.addEventListener('click', function () {
     // ASSINCRONA
     fetch('https://jsonplaceholder.typicode.com/posts/1')
-        .then((response) => response.json())
-        .then((json) => console.log(json));
+
+    .then(function (response) {
+        return response.json();
+    })
+
+    .then(function (json) {
+        console.log(json);
+    })
     // console.log("Clicou!");
 });
 
