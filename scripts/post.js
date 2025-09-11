4064b732a371b437762e9e44ba5ac6dbbb51e615
Melyssa Santos <salvadormelyssa2@gmail.com>
2025-09-11 09:00:05 -0300

    Chamando a classe no javascript

--- scripts/post.js
+++ scripts/post.js
@@ -1,1 +1,17 @@
-class Post {}
+// Data Model (Modelo de dados)
+class Post {
+    // Constructor serve para chamar a classe
+    constructor() {
+        this.userId;
+        this.id;
+        this.title;
+        this.body;
+    }
+}
+
+// // {
+//     "userId": 1,
+//     "id": 1,
+//     "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+//     "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+// }
