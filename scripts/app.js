94e7b9d73077da6f4aed738dbec95ce1db1197c1
Melyssa Santos <salvadormelyssa2@gmail.com>
2025-09-11 11:51:04 -0300

    Adicionando últimas informações

--- scripts/app.js
+++ scripts/app.js
@@ -5,10 +5,14 @@ button.addEventListener('click', function () {
     // ASSINCRONA
     fetch('https://jsonplaceholder.typicode.com/posts/88')
         .then(function (response) {
-            return response.json();
+            if (response.ok) {
+                return response.json();
+            } else {
+                let errorMensage = "Erro: Recurso não encontrado"
+                throw new Error("errorMensage")
+            }
         })
         .then(function (json) {
-            // div.innerText = json.body;
 
             // div.innerText = JSON.stringify(json, null, 0);
             // É um método da classe JSON - STRINGFY
@@ -16,4 +20,17 @@ button.addEventListener('click', function () {
             let post = Post.fromRaw(json);
             div = post.renderFrom(div);
         })
+        .catch(function (error) {
+            renderError(error);
+        })
 });
+
+function renderError(error) {
+    div.innerHTML = "";
+
+    let h2 = document.createElement(`h2`);
+    h2.textContent = error;
+
+    div.appendChild(h2);
+}
+
